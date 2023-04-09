603b8bc1b0f9f887505c4dcb6fca0ec8c6bfee1b
Niki <39004156+iByNiki@users.noreply.github.com>
2023-04-09 12:41:24 +0200

    .

--- assets/js/manager.js
+++ assets/js/manager.js
@@ -13,7 +13,7 @@ pathName = window.location.pathname.toLowerCase();
 
 if (cssLinks[pathName] !== undefined) {
 
-    console.log('<link rel="stylesheet" href="' + cssLinks[pathName] + '">');
-    document.write('<link rel="stylesheet" href="' + cssLinks[pathName] + '">');
+    console.log('<link rel="stylesheet" type="text/css" href="' + cssLinks[pathName] + '">');
+    document.write('<link rel="stylesheet" type="text/css" href="' + cssLinks[pathName] + '">');
 
 }
